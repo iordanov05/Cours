7a38d8d3132a92f7b8a0b68a88071907be2e8d4c
Warymor <iordanov05@mail.ru>
2023-11-12 17:18:29 +0300

    move BeerDTO to file

--- Course/Course/BeerTable/ViewController.swift
+++ Course/Course/BeerTable/ViewController.swift
@@ -48,28 +48,7 @@ final class ViewController: UIViewController, UITableViewDataSource {
         ])
         
         
-        let url: URL = URL(string: "https://api.punkapi.com/v2/beers")!
-        URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
-            guard
-                let data = data,
-                error == nil
-            else {
-                return
-            }
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            self.beerData = try! decoder.decode([BeerDTO].self, from: data)
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-        }).resume()
-        // Do any additional setup after loading the view.
+       
+       
     }
 }
-struct BeerDTO: Decodable{
-    let id: Int
-    let name: String
-    let tagline: String
-    let imageUrl: URL
-}
-
